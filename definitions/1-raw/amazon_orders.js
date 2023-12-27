187d57c0de803837142af4bd998598f7d18d588b
Gareth <cadmang@gmail.com>
2023-12-27 17:56:39 +0000

    update

--- definitions/1-raw/amazon_orders.js
+++ definitions/1-raw/amazon_orders.js
@@ -1,192 +1,40 @@
-const schemaString = {
-	"fields": [
-	{
-		"name": "data",
-		"type": "record",
-		"fields": [{
-			"name": "amazonOrderId",
-			"type": "STRING"
-		},
-		{
-			"name": "sellerOrderId",
-			"type": "STRING"
-		},
-		{
-			"name": "purchaseDate",
-			"type": "TIMESTAMP"
-		},
-		{
-			"name": "lastUpdateDate",
-			"type": "TIMESTAMP"
-		},
-		{
-			"name": "orderStatus",
-			"type": "STRING"
-		},
-		{
-			"name": "fulfillmentChannel",
-			"type": "STRING"
-		},
-		{
-			"name": "salesChannel",
-			"type": "STRING"
-		},
-		{
-			"name": "shipServiceLevel",
-			"type": "STRING"
-		},
-		{
-			"name": "orderTotal",
-			"type": "record",
-			"fields": [{
-				"name": "currencyCode",
-				"type": "STRING"
-			},
-			{
-				"name": "amount",
-				"type": "STRING"
-			}
-			]
-		},
-		{
-			"name": "numberOfItemsShipped",
-			"type": "INTEGER"
-		},
-		{
-			"name": "numberOfItemsUnshipped",
-			"type": "INTEGER"
-		},
-		{
-			"name": "paymentMethod",
-			"type": "STRING"
-		},
-		{
-			"name": "paymentMethodDetails",
-			"type": "STRING",
-			"mode": "repeated"
-		},
-		{
-			"name": "marketplaceId",
-			"type": "STRING"
-		},
-		{
-			"name": "shipmentServiceLevelCategory",
-			"type": "STRING"
-		},
-		{
-			"name": "orderType",
-			"type": "STRING"
-		},
-		{
-			"name": "earliestShipDate",
-			"type": "TIMESTAMP"
-		},
-		{
-			"name": "latestShipDate",
-			"type": "TIMESTAMP"
-		},
-		{
-			"name": "isBusinessOrder",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "isPrime",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "isPremiumOrder",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "isGlobalExpressEnabled",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "isReplacementOrder",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "isSoldByAB",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "isISPU",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "isAccessPointOrder",
-			"type": "BOOLEAN"
-		},
-		{
-			"name": "shippingAddress",
-			"type": "record",
-			"fields": [{
-				"name": "city",
-				"type": "STRING"
-			},
-			{
-				"name": "stateOrRegion",
-				"type": "STRING"
-			},
-			{
-				"name": "postalCode",
-				"type": "STRING"
-			},
-			{
-				"name": "countryCode",
-				"type": "STRING"
-			}
-			]
-		},
-		{
-			"name": "buyerInfo",
-			"type": "record",
-			"fields": [{
-				"name": "buyerEmail",
-				"type": "STRING"
-			}]
-		},
-		{
-			"name": "hasRegulatedItems",
-			"type": "BOOLEAN"
-		}
-		]
-	}
-	]
-};
+const schemaString = require("./amazon_orders_schema.json");
+const tableName = "amazon_orders"
+const schema = "raw"
+const uri = "gs://<BUCKET_NAME>/AMAZON/ORDERS/"
+const forceFullDataLoad = dataform.projectConfig.vars.forceFullDataLoad;
+const currentDate = forceFullDataLoad === 'true' ? '' : functions.getCurrentDate();
+const overrideLoadDate = dataform.projectConfig.vars.overrideLoadDate;
+const loadDate = overrideLoadDate ? overrideLoadDate : currentDate;
+const overwritePartitions = forceFullDataLoad === 'true' ? '' : OVERWRITE PARTITIONS (dt = '${loadDate}');
 
-var table_name = "amazon_orders"
-var schema = "raw"
-var uri = "gs://<BUCKET_NAME>/AMAZON/ORDERS/"
 
-operate(table_name)
+operate(tableName)
 	.tags(['amazon'])
 	.schema(schema)
 	.hasOutput(true)
 	.queries(
 		(ctx) => `
-BEGIN
-       LOAD DATA INTO ${ctx.resolve(schema, table_name)} 
-			(\n${macros.formatJsonSchema(schemaString)}\n)
-       OVERWRITE PARTITIONS (dt='${macros.getCurrentDate()}')
-       PARTITION BY dt 
-	   OPTIONS (description = 'Amazon Orders via https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference#order')
-       FROM FILES (
-			format = 'JSON',
-        	hive_partition_uri_prefix = '${uri}',
-        	uris = ['${uri}dt=${macros.getCurrentDate()}*.data'],
+    BEGIN
+		LOAD DATA INTO ${ctx.resolve(schema, tableName)}
+		(\n${functions.formatJsonSchema(schemaString)}\n)
+		${overwritePartitions}
+		PARTITION BY dt 
+		OPTIONS (description = 'Amazon Orders via https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference#order')
+		FROM FILES ( format = 'JSON',
+			hive_partition_uri_prefix = '${uri}',
+			uris = ['${uri}dt=${loadDate}*.data'],
 			ignore_unknown_values = TRUE 
 		)
-       WITH PARTITION COLUMNS(dt DATE);
+		WITH PARTITION COLUMNS(dt DATE);
 
-EXCEPTION WHEN ERROR THEN
-IF @@error.message like '%cannot query hive partitioned data for table%without any associated files%' THEN
-SELECT (@@error.message) as message;
-ELSE
-RAISE USING MESSAGE = (@@error.message);
-END IF;
-END;
+	EXCEPTION WHEN ERROR THEN
+	IF @@error.message like '%cannot query hive partitioned data for table%without any associated files%' THEN
+	SELECT (@@error.message) as message;
+	ELSE
+	RAISE USING MESSAGE = (@@error.message);
+	END IF;
+	END;
+    `);
 
-`
-);
 
