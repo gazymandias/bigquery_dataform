6a0a21374dc3dbbd6d15a6aa6daedfd2fafdb4bb
Gareth Cadman <48953362+gazymandias@users.noreply.github.com>
2024-02-04 22:31:22 +0000

    Update amazon_orders.js

--- definitions/1-raw/amazon_orders.js
+++ definitions/1-raw/amazon_orders.js
@@ -10,32 +10,66 @@ const overwritePartitions = forceFullDataLoad === 'true' ? '' : `OVERWRITE PARTI
 const overwriteOrInto = forceFullDataLoad === 'true' ? 'OVERWRITE' : 'INTO';
 
 
-operate(tableName)
+
+
+operate(tableName+'_speed')
 	.tags(['amazon'])              
 	.schema(schema)
 	.hasOutput(true)
 	.queries(
 		(ctx) => `
-    BEGIN
-		LOAD DATA ${overwriteOrInto} ${ctx.resolve(schema, tableName)}
-		(\n${functions.formatJsonSchema(schemaString)}\n)
+  		BEGIN
+		CREATE OR REPLACE EXTERNAL TABLE
+			${ctx.resolve(schema, tableName + '_speed')}
+		WITH PARTITION COLUMNS ( dt DATE) OPTIONS ( 
+  			format = 'JSON',
+  		    	hive_partition_uri_prefix = '${uri}',
+			uris = ['${uri}dt=${loadDate}*.data'],
+			ignore_unknown_values = TRUE 
+		  	require_hive_partition_filter = FALSE
+		);
+
+		EXCEPTION WHEN ERROR THEN
+		IF lower(@@error.message) like '%cannot query hive partitioned data for table%without any associated files%' THEN
+		SELECT (@@error.message) as message;
+		ELSE
+		RAISE USING MESSAGE = (@@error.message);
+		END IF;
+		END;
+    `);
+
+
+
+operate(tableName+'_batch')
+	.tags(['amazon'])              
+	.schema(schema)
+	.hasOutput(true)
+	.queries(
+		(ctx) => `
+		BEGIN
+		LOAD DATA ${overwriteOrInto} 
+  			${ctx.resolve(schema, tableName + '_batch')}
+		(
+  			\n${functions.formatJsonSchema(schemaString)}\n
+     		)
 		${overwritePartitions}
 		PARTITION BY dt 
 		OPTIONS (description = 'Amazon Orders via https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference#order')
-		FROM FILES ( format = 'JSON',
+		FROM FILES ( 
+  			format = 'JSON',
 			hive_partition_uri_prefix = '${uri}',
 			uris = ['${uri}dt=${loadDate}*.data'],
 			ignore_unknown_values = TRUE 
 		)
 		WITH PARTITION COLUMNS(dt DATE);
 
-	EXCEPTION WHEN ERROR THEN
-	IF lower(@@error.message) like '%cannot query hive partitioned data for table%without any associated files%' THEN
-	SELECT (@@error.message) as message;
-	ELSE
-	RAISE USING MESSAGE = (@@error.message);
-	END IF;
-	END;
+		EXCEPTION WHEN ERROR THEN
+		IF lower(@@error.message) like '%cannot query hive partitioned data for table%without any associated files%' THEN
+		SELECT (@@error.message) as message;
+		ELSE
+		RAISE USING MESSAGE = (@@error.message);
+		END IF;
+		END;
     `);
 
 
